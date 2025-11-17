8bc763b4c02d2178e7d861791bfce3cdb2ed190e
Matheus-Ayres <matheussayres9@gmail.com>
2025-11-17 14:56:35 -0300

    Interface

---
+++ interface/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Carro carro = new Carro();
+
+        carro.ligar();
+        carro.acelerar();
+    }
+}
---
+++ interface/src/Carro.java
@@ -0,0 +1,10 @@
+public class Carro implements Veiculo {
+    
+    public void ligar(){
+        System.out.println("Carro ligado");
+    }
+
+    public void desligar(){ System.out.println("Carro desligado"); }
+    public void acelerar(){ System.out.println("Carro acelerando"); }
+
+}
